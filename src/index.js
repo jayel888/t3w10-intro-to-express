c31843f36e5ede91169da3a1e759e87420f9b1a2
jayel888 <15644@coderacademy.edu.au>
2024-12-17 20:31:17 +1100

    feat: added POST request

--- src/index.js
+++ src/index.js
@@ -11,9 +11,11 @@ const PORT = 3000;
 
 app.get("/", (req, res) => {
     // Function logic executed
-    
     // res.send("<h1>Hello from Express Backend!</h1>");
+    // res.sendStatus(201);
+    // res.download('./src/index.js');
     res.status(201).send("<h1>Hello from Express Backend!</h1>");
+    
 })
 
 app.get("/users", (req, res) => {
@@ -24,17 +26,13 @@ app.get("/users", (req, res) => {
     });
 })
 
-app.get("/", (req, res) => {
-    // Function logic executed
-    
-    res.send("<h1>Hello from Express Backend!</h1>");
-})
 
-app.get("/", (req, res) => {
+app.post("/", (req, res)=> {
     // Function logic executed
-    
-    res.send("<h1>Hello from Express Backend!</h1>");
-})
+    res.json({
+        message: "POST Request made!"
+    });
+});
 
 
 app.listen(PORT, () => {
