4b0fd9d94f8b4033719ea34ef42e0dd156481789
jayel888 <15644@coderacademy.edu.au>
2024-12-19 19:42:26 +1100

    feat: Separated server.js and index.js, defined PORT value as an env variable

--- src/index.js
+++ src/index.js
@@ -1,34 +1,7 @@
-const express = require("express");
-
-const router = require("../routes/users")
-
-
-// Instance of the express for configuration
-const app = express();
+const { app } = require("./server")
 
 // Define a default PORT number
-const PORT = 3000;
-
-// Our URL: localhost:PORTNUMBER/url_name
-// app.get or post("url", callback);
-
-app.get("/", (req, res) => {
-    // Function logic executed
-    // res.send("<h1>Hello from Express Backend!</h1>");
-    // res.sendStatus(201);
-    // res.download('./src/index.js');
-    res.status(201).send("<h1>Hello from Express Backend!</h1>");
-    
-})
-
-app.post("/", (req, res)=> {
-    // Function logic executed
-    res.json({
-        message: "POST Request made!"
-    });
-});
-
-app.use("/users", router);
+const PORT = parseInt(process.env.PORT) ?? 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`)
